95ba06449e8c58653c7bab5d8b84f4f27f79e5f7
Isaac Li <lee_isaac@163.com>
2016-04-21 16:34:01 +0800

    new project-first commit

---
+++ ServletTest/src/main/java/com/isaac/javaweb/ServletTest.java
@@ -0,0 +1,43 @@
+package com.isaac.javaweb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletTest extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4140553978112858697L;
+
+	/**
+	 * 
+	 */
+
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+		System.out.println("doGet method");
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#service(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+	}
+	
+	
+
+}
